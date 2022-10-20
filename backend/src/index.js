8a4fd9da90c4e508cd5258427ff553e30f6930de
Dhruv-m-Shah <shahdhruv2001@gmail.com>
2022-10-20 02:47:21 -0400

    finished /api/table endpoint for filtering games

--- backend/src/index.js
+++ backend/src/index.js
@@ -52,16 +52,59 @@ app.get("/api/table", async (req, res) => {
     const minElo = req.query.minElo || 0;
     const event = req.query.event;
     const result = req.query.result;
-    console.log(whitePlayer, blackPlayer, minElo, event, result);
-    // const results = await query({
-    //     sql: ``,
-    //     values: [],
-    //     nestTables: true
-    // });
-    // res.send(results);
+    console.log(event == "");
+    params = [whitePlayer, blackPlayer, event, result];
+    values = [minElo, minElo];
+    let sqlQuery = "SELECT * FROM games";
+    sqlQuery += " JOIN players AS white ON games.white_id=white.id";
+    sqlQuery += " JOIN players AS black ON games.black_id=black.id";
+
+    sqlQuery += " WHERE (games.white_elo >= ? OR games.black_elo >= ?)";
+
+    // Dynamically modify query based on what filter parameters were given
+    // If parameter is empty string it was not given by user, so we do not filter on it.
+    if(whitePlayer != ""){
+          sqlQuery += " AND white.name = ?";
+          values.push(whitePlayer);
+    }
+    if(blackPlayer != ""){
+        sqlQuery += " AND black.name = ?";
+        values.push(blackPlayer);
+    }
+    if(event != "") {
+        sqlQuery += " AND games.event = ?";
+        values.push(event);
+    }
+    if(result != "") {
+        sqlQuery += " AND games.result = ?";
+        values.push(result);
+    }
+
+    const results = await query({
+         sql: sqlQuery,
+         values: values,
+         nestTables: true
+     });
+
+     formattedResults = []
+     for(let i = 0; i < results.length; i++){
+        entry = {}
+        entry.id = results[i].games.id;
+        entry.date = results[i].games.date;
+        entry.white_player = results[i].white.name;
+        entry.white_elo = results[i].games.white_elo;
+        entry.black_player = results[i].black.name;
+        entry.black_elo = results[i].games.white_elo;
+        entry.result = results[i].games.result;
+        entry.event = results[i].games.event;
+        entry.site = results[i].games.site;
+        formattedResults.push(entry);
+     }
+     res.send(formattedResults);
 
     // *game_id should be sent as id for the frontend
-    res.send([{id: 1, date: "2020-01-01", white_player: "Magnus Carlsen", white_elo: 1000, black_player: "Hans Neimann", black_elo: 1000, result: "black", event: "event", site: "site"}]);
+    // FORMAT OF HOW TO SEND RESULTS TO FRONTEND:
+    // res.send([{id: 1, date: "2020-01-01", white_player: "Magnus Carlsen", white_elo: 1000, black_player: "Hans Neimann", black_elo: 1000, result: "black", event: "event", site: "site"}]);
 });
 
 app.listen(process.env.PORT || 5000);
