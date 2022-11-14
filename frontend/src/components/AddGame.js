b671034ba5e778a309acd926109fcb66b46caccb
matt <m28benne@uwaterloo.ca>
2022-11-14 17:31:25 -0500

    Allow for adding multiple games at once

--- frontend/src/components/AddGame.js
+++ frontend/src/components/AddGame.js
@@ -3,31 +3,33 @@ import axios from 'axios';
 
 function AddGame() {
 
-  const [file, setFile] = useState()
+  const [files, setFiles] = useState()
 
   function handleChange(event) {
-    setFile(event.target.files[0])
+    setFiles(event.target.files)
   }
   
   function handleSubmit(event) {
-    event.preventDefault()
-    const url = 'http://localhost:5000/addGame';
-    var reader = new FileReader();
-
-    reader.onload = function(event) {
-        axios.post(url, {"str": event.target.result}).then(function (response) {
-            console.log(response);
-          })
-    };
-
-    reader.readAsText(file);
+    for (var file of files) {
+      event.preventDefault()
+      const url = 'http://localhost:5000/addGame';
+      var reader = new FileReader();
+  
+      reader.onload = function(event) {
+          axios.post(url, {"str": event.target.result}).then(function (response) {
+              console.log(response);
+            })
+      };
+  
+      reader.readAsText(file);
+    }
   }
 
   return (
     <div className="AddGame">
         <form onSubmit={handleSubmit}>
           <h1>Add Game</h1>
-          <input type="file" onChange={handleChange}/>
+          <input type="file" onChange={handleChange} multiple/>
           <button type="submit">Upload</button>
         </form>
     </div>
